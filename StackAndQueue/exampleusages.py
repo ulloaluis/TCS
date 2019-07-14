1af00522b76a222e4c561eea9c7bd78b1c74b6b6
Luis Ulloa <ulloa@stanford.edu>
2019-07-14 14:17:16 -0400

    Create exampleusages.py

---
+++ StackAndQueue/exampleusages.py
@@ -0,0 +1,50 @@
+from StackAndQueue.py import Stack, Queue
+
+# Example usages:        
+
+def reverse(stack):
+    tempQueue = Queue()
+    while not stack.isEmpty():
+        tempQueue.enqueue(stack.pop())
+    while not tempQueue.isEmpty():
+        stack.push(tempQueue.dequeue())
+
+s = Stack([1, 2, 3])
+print(s)
+reverse(s)
+print(s)
+
+
+def removeBottom(stack): # intentionally buggy code, answer: reverse order
+    tempQueue = Queue()
+    while not stack.isEmpty():           
+        element = stack.pop();
+        if stack.size() == 0: #if last element was popped, don't queue it
+            break
+        tempQueue.enqueue(element);
+    while not tempQueue.isEmpty():
+        stack.push(tempQueue.dequeue()) #put elements back in stack
+        
+s = Stack([1, 2, 3])
+print(s)
+removeBottom(s)
+print(s)
+
+def printStack(stack):
+  #how does this temp stack help replace the original? Draw it out!
+    temp = Stack()
+    while not stack.isEmpty():
+        temp.push(stack.peek())
+        print(stack.peek())  #could also say print temp.peek() 
+        stack.pop()
+    while not temp.isEmpty():
+        stack.push(temp.pop())
+s = Stack([1, 2, 3])
+print(s)
+printStack(s)
+
+stack = Stack([3, 4, 5])
+product = stack.pop()
+while not stack.isEmpty():
+  product *= stack.pop()
+print(product)
