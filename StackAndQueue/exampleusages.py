8036b872e825551a9fa34a16c68f60167a3c0170
Luis Ulloa <ulloa@stanford.edu>
2019-07-14 14:20:12 -0400

    Update exampleusages.py

--- StackAndQueue/exampleusages.py
+++ StackAndQueue/exampleusages.py
@@ -1,4 +1,4 @@
-from StackAndQueue.py import Stack, Queue # With StackAndQueue.py in the same directory
+from StackAndQueue import Stack, Queue # With StackAndQueue.py in the same directory
 
 # Example usages:        
 
